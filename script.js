5dda8c356d228b669646d3c3f22e0a1add464f9c
Rainyflix <Nicholas.Hole@gmail.com>
2026-09-18 22:39:12 +0200

    Disable right click on website

--- script.js
+++ script.js
@@ -108,3 +108,8 @@ if (bouncingCharacter) {
     );
   });
 }
+
+// Disable right-click context menu on the entire page
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault();
+});
